1bb864ee9579c9e4eee3f3a13afd3862e02fcc1b
Ritwik <mritwik369@gmail.com>
2021-04-11 20:50:29 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-var map = L.map('mapid').setView([19.075983, 72.877655], 12);
+Array.prototype.unique = function () {
+    var arr = [];
+    for (var i = 0; i < this.length; i++) {
+        if (!arr.contains(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+}
+
+async function reverseGeoCode(lat, long) {
+    return await (await fetch(`https://us1.locationiq.com/v1/reverse.php?key=bea4a9ec9a2ceb&lat=${lat}&lon=${long}&format=json`)).json();
+}
+
+async function getCoords() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
+            L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+        });
+    }
+}
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1 / 180
+    var radlat2 = Math.PI * lat2 / 180
+    var radlon1 = Math.PI * lon1 / 180
+    var radlon2 = Math.PI * lon2 / 180
+    var theta = lon1 - lon2
+    var radtheta = Math.PI * theta / 180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180 / Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
+    return dist
+}
+
+var map = L.map('mapid').setView([19.075983, 72.877655], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -11,19 +50,36 @@ var opl = new L.OverPassLayer({
         console.log(data);
         let elements = data.elements;
 
+        let resp = [];
+
         elements.forEach(el => {
-            // console.log(el);
             if (typeof el.geometry !== "undefined") {
                 let latlngs = el.geometry.map(el => [el.lat, el.lon]);
                 var polygon = L.polygon(latlngs, { color: 'red' }).addTo(map);
+
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(position => {
+                        let ll = el.geometry.map(el => [el.lat, el.lon]);
+                        ll.forEach(async c => {
+                            // console.log(c);
+                            let dist = distance(c[0], c[1], position.coords.latitude, position.coords.longitude, "K");
+                            if (dist < 5) {
+                                setTimeout(async () => {
+                                    let location = await reverseGeoCode(c[0], c[1]);
+                                    if (typeof location.error === "undefined")
+                                    resp.push(location);
+                                }, 2000); 
+                            }
+                        })
+                        // console.log("dist: ", dist);
+                    });
+                }
             }
         });
 
-        // create a red polygon from an array of LatLng points
-        // var latlngs = [[37, -109.05], [41, -109.03], [41, -102.05], [37, -102.04]];
-        // var polygon = L.polygon(latlngs, { color: 'red' }).addTo(map);
-        // zoom the map to the polygon
-        // map.fitBounds(polygon.getBounds());
+        console.log(resp);
+
+        // areas = elements;
     },
 });
 
